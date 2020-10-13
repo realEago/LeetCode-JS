60a33cf7804638353466c79c7f7f5a4e41fd4e04
zhaiyuanhang <zhaiyuanhang@xylink.com>
2020-10-13 08:21:15 +0800

    study: 算法

---
+++ 排序算法/020216常见排序算法理解与JS实现.js
@@ -0,0 +1,101 @@
+//https://www.jianshu.com/p/a87b3d0eeab5
+// 0.什么是算法？
+// 输入：一个算法必须有零个或以上输入量。
+// 输出：一个算法应有一个或以上输出量。
+// 明确性：算法的描叙必须无歧义，实际运行结果是确定的
+// 有限性：必须在有限个步骤内结束 有效性：又称可行性。能够被执行者实现。
+// ————高德纳《计算机程序设计艺术》
+// 先定义好互换函数Swap，作用为交换数组中两个数的值:
+//
+function swap(array, a, b) {
+  var temp = array[a]
+  array[a] = array[b]
+  array[b] = temp
+}
+
+// 1.冒泡排序
+// 冒泡排序可以理解为，遍历整个数组，同时每次将两个数进行比较，大的往后放小的往前放。这样一定次数后，大的就会渐渐向后聚集，从而实现排序的效果。
+// JS实现：
+
+function sort(array) {
+  var i
+  var j
+  for (i = 1; i < array.length; i++) {
+    for (j = 0; j < array.length - i; j++) {
+      if (array[j] <= array[j + 1]) {
+      } else
+        swap(array, j, j + 1)
+      console.log("swap" + array[j] + ',' + array[j + 1])
+    }
+  }
+  return array;
+}
+
+// 2.选择排序
+// 选择排序：先将指针指向第一个元素i，并标记它为最小元素indexOfmin，定义它后面的一个元素为j.然后将i自增并遍历整个数组,只要第j个元素小于标记的最小元素，就将j的值赋给最小元素。同时，只要最小元素与i相等，就将最小元素与 i互换。
+
+function sort(array) {
+  var i
+  var j
+  var indexOfMin
+  for (i = 0; i < array.length; i++) {
+    indexOfMin = i
+    for (j = i + 1; j < array.length; j++) {
+      if (array[j] < array[indexOfMin]) {
+        indexOfMin = j
+      }
+    }
+    if (indexOfMin !== i) {
+      swap(array, i, indexOfMin)
+    }
+  }
+  return array;
+}
+
+// 3.插入排序
+// 插入排序的理解比较简单，可以简略的理解为打牌的时候起牌的方法：我手里已经有一副牌，每抓到一张新牌（读取新值）就和我手里的牌进行比较，大的话就放右边，小的话就放左边。首先我将要插入的数放在数组第一个位置，然后从最后一个位置开始从右向左查找。每当我从右开始读取的数比需要插入的数大，我就将其右移一位。这样既可花费n-1步找到需要排序的位置。
+// JS代码实现：
+
+function sort(array) {
+  var i
+  var j
+  for (i = 1; i < array.length; i++) {
+    console.log('current:' + i + ':' + array[i])
+    var position = i
+    for (j = i - 1; j >= 0; j--) {
+      if (array[j] > array[i]) {
+        position = j
+        console.log('position:' + j)
+      } else {
+        var value = array[position]
+        array.splice(position, 1)
+        array.splice(j, 0, value)
+        console.log('insert at :' + j)
+      }
+    }
+  }
+  return array;
+}
+
+// 4.快速排序
+// 快速排序又称为自私算法，它优先让每个元素找到自己所在的位置，每次排序都实现“比我大的都在我右边，比我小的都在我左边”而不去计较它们的位置关系。具体做法为：先找一个基准点（一般用第一个元素或者中间元素）然后数组被分为两部分，如果选定值比它小，放左边；比它大，放右边。然后进行反复比较，就可以实现效果。
+
+function sort(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+  var len = Math.floor(array.length / 2);
+  var cur = array.splice(len, 1);
+  var left = [];
+  var right = [];
+  for (var i = 0; i < array.length; i++) {
+    if (cur > array[i]) {
+      left.push(array[i]);
+    } else {
+      right.push(array[i]);
+    }
+  }
+  return sort(left).concat(cur, sort(right));
+}
+
+
---
+++ 排序算法/排序算法⭐️.js
@@ -0,0 +1,133 @@
+//交换函数
+function swap(ary,a,b) {
+  ary[a] = ary[b] + ary[a]
+  ary[b] = ary[a] - ary[b]
+  ary[a] = ary[a] - ary[b]
+}
+
+//1、冒泡排序
+//2、选择排序
+//3、插入排序/BST
+//4、归并排序
+//5、快速排序
+
+
+//冒泡排序 最优写法，无法再优化写法
+//冒泡排序算法的运作如下：
+// 1.比较相邻的元素。如果第一个比第二个大，就交换他们两个。
+// 2.对每一对相邻元素作同样的工作，从开始第一对到结尾的最后一对。这步做完后，最后的元素会是最大的数。
+// 3.针对所有的元素重复以上的步骤，除了最后一个。
+// 4.持续每次对越来越少的元素重复上面的步骤，直到没有任何一对数字需要比较。
+
+
+//时间复杂度 O(n^2)
+//空间复杂度 O(1)
+function bubbleSort(ary) {
+  for (let a = ary.length - 1;a > 0;a--) { //最后一位不扫描,扫描次数为数组元素个数减一
+    var swapped = false   //记录是否交换过
+    for (let b = 0; b < a;b++) {
+      if (ary[b] > ary[b+1]) {
+        swap(ary,b,b+1)
+        swapped = true
+      }
+    }
+    if (!swapped) {
+      return ary  //如果一次都没有交换过则说明这个次序已经是从小到大的
+    }
+  }
+  return ary
+}
+bubbleSort([2,4,5,6,7,2,21,5])
+
+
+//选择排序
+//选择排序（Selection sort）是一种简单直观的排序算法。它的工作原理如下。首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置，然后，再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。以此类推，直到所有元素均排序完毕
+//算法复杂度 O(n^2)
+
+function selectSort(ary) {
+  for (let a = 0;a < ary.length - 1;a++) { //最后两个元素比出比较小的那个即可
+    let minIndex = a
+    for (let b = a + 1;b < ary.length;b++) {
+      if(ary[b] < ary[minIndex]){
+        minIndex = b
+      }
+    }
+    if(minIndex != a) {
+      swap(ary,a,minIndex)
+    }
+  }
+  return ary
+}
+
+
+//归并排序
+//时间复杂度为O(N*logN)
+//空间复杂度为O(N)
+// 1.把 n 个元素看成 n 个长度为 l 的有序子表
+// 2.进行两两归并使记录关键字有序，得到 n/2 个长度为 2 的有序子表
+// 3.重复第 2 步直到所有记录归并成一个长度为 n 的有序表为止。
+function mergeSort(ary) {
+  if(ary.length < 2) {
+    return ary
+  }
+  var middle = parseInt(ary.length / 2) //相当于 Math.floor()
+  var left = ary.slice(0,middle)
+  var right = ary.slice(middle)
+  return merge(mergeSort(left),mergeSort(right))
+}
+function merge(left,right) {
+  var result = []
+  var i = 0; j = 0;
+  while(i < left.length && j < right.length) {
+    if (left[i] > right[j]) {
+      result.push(right[j])
+      j++
+    } else {
+      result.push(left[i])
+      i++
+    }
+  }
+  while(i < left.length) {
+    result.push(left[i])
+    i++
+  }
+  while(j < right.length) {
+    result.push(right[j])
+    j++
+  }
+  return result
+ }
+//快速排序
+//（1）在数据集之中，选择一个元素作为"基准"（pivot）。
+//（2）所有小于"基准"的元素，都移到"基准"的左边；所有大于"基准"的元素，都移到"基准"的右边。
+//（3）对"基准"左边和右边的两个子集，不断重复第一步和第二步，直到所有子集只剩下一个元素为止。
+function quickSort(ary) {
+  if(ary.length < 2) {
+    return ary
+  }
+  var markIndex = parseInt(ary.length / 2)
+  var mark = ary.splice(markIndex,1)[0]
+  var left = []
+  var right = []
+  for (var i = 0; i < ary.length;i++) {
+    if (ary[i] < mark) {
+      left.push(ary[i])
+    } else {
+      right.push(ary[i])
+    }
+  }
+  return quickSort(left).concat([mark],quickSort(right))
+}
+
+
+
+// 排序算法的稳定性：
+//    指排序前后，相同元素的相对位置是否发生变化
+//    如果发生变化，则称此排序算法为不稳定的排序算法
+//    如果不发生变化，则称此排序算法为稳定的排序算法
+
+// 冒泡排序：稳定（可以写成不稳定）
+// 选择排序：不稳定
+// BST/插入排序：稳定
+// 归并排序：稳定（可以写成不稳定）
+// 快速排序：不稳定
